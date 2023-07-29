a95f3c81e4221d14a658273c00cf9eb4de4f8d44
Nicholas Mercadante <25127906+dantayy@users.noreply.github.com>
2023-07-29 16:45:28 -0400

    FPS starter content

---
+++ Source/GrapplingHook/GrapplingHookGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrapplingHookGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGrapplingHookGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGrapplingHookGameMode();
+};
+
+
+
